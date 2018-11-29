bf0eac1e36c8377c93cde1af629201b2fb93a1c2
BrandonBain23 <find@A5-004098.localdomain>
2018-11-28 17:18:44 -0700

    starter code

---
+++ app.js
@@ -0,0 +1,9 @@
+// Countdown to dec 25
+
+// HTML element grabber
+// get date for today
+// get date we want
+// calculation, to know how much longer till that day
+// display on HTML
+
+// eventlistenter, onchange, the of difference calculation
